d79507919372dc429c8d1480e71357a3af77d69d
tuanpm3435 <57953001+BlackCattttttt@users.noreply.github.com>
2024-07-05 14:19:49 +0700

    add behavior tree package

---
+++ Unimob/Assets/Plugins/ParadoxNotion/NodeCanvas/Tasks/Conditions/Blackboard/CheckNull.cs
@@ -0,0 +1,22 @@
+﻿using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+
+
+namespace NodeCanvas.Tasks.Conditions
+{
+
+    [Category("✫ Blackboard")]
+    [Description("Check whether or not a variable is null")]
+    public class CheckNull : ConditionTask
+    {
+
+        [BlackboardOnly]
+        public BBParameter<System.Object> variable;
+
+        protected override string info => $"{variable} == null";
+
+        protected override bool OnCheck() {
+            return variable.value == null;
+        }
+    }
+}
